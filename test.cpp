646b590275c7a84e7a6ef9cc2fb0aca05b693f0a
RrReyad_HomeWindows <rr.reyad420@gmail.com>
2023-01-13 21:26:41 +0600

    1st commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"this is the first test\n";
+    cout <<"Learning \"cpp\"\n";
+    cout<<"Enter name: \n";
+    cin>>name;
+    cout<<"Name is:" +name;
+    return 0;
+}
